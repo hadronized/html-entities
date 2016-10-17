1725697fb4d5a403618787eeeb4f9a4074727a00
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2016-10-17 19:41:04 +0200

    Added support for numeric html entities.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 #[macro_use]
 extern crate lazy_static;
 
+use std::char::from_u32;
 use std::collections::BTreeMap;
+use std::iter::Peekable;
 use std::str::Chars;
 
 mod entities;
@@ -38,15 +40,22 @@ type Line = usize;
 type Col = usize;
 
 pub fn decode_html_entities(html: &str) -> Result<String, DecodeError> {
-  let mut chars = html.chars(); // iterator over the HTML to decode
+  let mut chars = html.chars().peekable(); // iterator over the HTML to decode
   let mut decoded = String::new(); // decoded string
   let mut line = 1; // current line
   let mut col = 1; // current column
 
   while let Some(c) = chars.next() {
     if c == '&' {
-      let entity = try!(parse_entity(&mut chars, line, &mut col));
-      let unicode_entity = try!(ENTITIES.entities.get(&*entity).ok_or(DecodeError::UnknownEntity(line, col, entity)));
+      let unicode_entity = if let Some(&'#') = chars.peek() {
+        chars.next();
+        try!(parse_entity_numeric(&mut chars, line, &mut col))
+      } else {
+        let entity = try!(parse_entity_name(&mut chars, line, &mut col));
+        let unicode = try!(ENTITIES.entities.get(entity.as_str()).ok_or(DecodeError::UnknownEntity(line, col, entity)));
+        (*unicode).to_owned()
+      };
+      
       decoded += &unicode_entity;
     } else {
       if c == '\n' {
@@ -62,7 +71,51 @@ pub fn decode_html_entities(html: &str) -> Result<String, DecodeError> {
   Ok(decoded)
 }
 
-fn parse_entity(chars: &mut Chars, line: usize, col: &mut usize) -> Result<String, DecodeError> {
+fn parse_entity_numeric(chars: &mut Peekable<Chars>, line: usize, col: &mut usize) -> Result<String, DecodeError> {
+  let c = match chars.peek() {
+    Some(&'x') | Some(&'X') => {
+      chars.next();
+      try!(parse_entity_hex(chars, line, col))
+    },
+    _ => try!(parse_entity_dec(chars, line, col))
+  };
+
+  let mut s = String::new();
+  s.push(c);
+  Ok(s)
+}
+
+fn parse_entity_hex(chars: &mut Peekable<Chars>, line: usize, col: &mut usize) -> Result<char, DecodeError> {
+  let num = try!(parse_number(chars));
+  let dec = try!(u32::from_str_radix(&num, 16).map_err(|_| DecodeError::IllFormedEntity(line, *col)));
+
+  from_u32(dec).ok_or(DecodeError::IllFormedEntity(line, *col))
+}
+
+fn parse_entity_dec(chars: &mut Peekable<Chars>, line: usize, col: &mut usize) -> Result<char, DecodeError> {
+  let num = try!(try!(parse_number(chars)).parse().map_err(|_| DecodeError::IllFormedEntity(line, *col)));
+  from_u32(num).ok_or(DecodeError::IllFormedEntity(line, *col))
+}
+
+fn parse_number(chars: &mut Peekable<Chars>) -> Result<String, DecodeError> {
+  let mut hex = String::new();
+
+  loop {
+    if let Some(c) = chars.next() {
+      if c == ';' {
+        break;
+      }
+
+      hex.push(c);
+    } else {
+      return Err(DecodeError::EOF)
+    }
+  }
+
+  Ok(hex)
+}
+
+fn parse_entity_name(chars: &mut Peekable<Chars>, line: usize, col: &mut usize) -> Result<String, DecodeError> {
   let mut entity = String::with_capacity(ENTITIES.max_html_length);
   let mut l = 0;
 
